f369277c399b9d6aa3be4544481eadebd9a023a6
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-06-25 17:10:59 +0100

    Added new speakers JS package for the _speakers collection

---
+++ assets/js/package-speakers.js
@@ -0,0 +1,10 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/owl.carousel
+//= require vendor/lazysizes
+//= require vendor/ls.unveilhooks
+
+//= require app/main
+//= require app/speakers
+//= require app/tables
