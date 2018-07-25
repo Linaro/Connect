9fc10702ed0d2eed80aeaf620c6b7af65abcb9c2
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-07-25 11:13:01 +0100

    Added additional JS files/packages

---
+++ assets/js/app/resources.js
@@ -0,0 +1,14 @@
+$(function() {
+    function count($this){
+        var current = parseInt($this.html(), 10);
+        $this.html(++current);
+        if(current !== $this.data('count')){
+            setTimeout(function(){count($this)}, 50);
+        }
+    }        
+  $("span.count-to-number").each(function() {
+      $(this).data('count', parseInt($(this).html(), 10));
+      $(this).html('0');
+      count($(this));
+  });
+});
