ded85950ebe0071e4def43ab82a34e110fa07f0a
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-07-30 16:21:52 +0100

    Added carousel for speakers

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -9,3 +9,26 @@ $(window).on("load",function(){
 
     });
 });
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+});
