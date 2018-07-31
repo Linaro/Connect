6ebd6e456006b785ccadc03c3725c647d1e772e5
Ciaran Moran <morancj@users.noreply.github.com>
2018-07-31 11:25:10 +0100

    Tag with short Git commit ID, change path for gemfiles

--- build.sh
+++ build.sh
@@ -16,36 +16,41 @@ DOCKER_MEM_LIMIT="1GB"
 # Allow 'docker run to use 75% CPU count
 DOCKER_RUN_CPU_COUNT="4"
     export DOCKER_RUN_CPU_COUNT
-# For Dockerfile development, date stamps are helpful
-# DOCKER_TAGS="linaro/connect:$DATETIME"
-DOCKER_TAGS="linaro/connect:latest"
-	export DOCKER_TAGS
+DOCKER_IMAGE_NAME="linaro/connect"
+	export DOCKER_IMAGE_NAME
+DOCKER_IMAGE_VERSION="$(git rev-parse --short HEAD)"
+	export DOCKER_IMAGE_VERSION
+# Alternative version string:
+# DOCKER_IMAGE_VERSION="$(git log -1 --pretty=%H)"
+# 	export DOCKER_IMAGE_VERSION
+DOCKER_TAG="$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION"
+	export DOCKER_TAG
 DOCKER_HOSTNAME="$(hostname)-connect-builder"
 	export DOCKER_HOSTNAME
 BUILD_DIR="$PWD"
 	export BUILD_DIR
-GEM_HOME="/srv/gems"
+GEM_HOME="/srv/.gemfiles"
 	export GEM_HOME
 
 function _re_build_docker_image() {
-    docker build --label "$DOCKERLABEL" --memory "$DOCKER_MEM_LIMIT" --rm -t "$DOCKER_TAGS" ./
+    docker build --rm --label "$DOCKERLABEL" --memory "$DOCKER_MEM_LIMIT" --rm -t "$DOCKER_TAG" ./
 }
 
 function docker_local_gem_install() {
-    docker run --rm -t -i --cpus="$DOCKER_RUN_CPU_COUNT" -e GEM_HOME="$GEM_HOME" -e HOME=srv -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -w /srv -v "$(pwd)":/srv --hostname="$DOCKER_HOSTNAME" "$DOCKER_TAGS" bundle install
+    docker run --rm -t -i --cpus="$DOCKER_RUN_CPU_COUNT" -e GEM_HOME="$GEM_HOME" -e HOME=srv -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -w /srv -v "$(pwd)":/srv --hostname="$DOCKER_HOSTNAME" "$DOCKER_TAG" bundle install
 }
 function docker_build_site() {
-    docker run --rm -t -i --cpus="$DOCKER_RUN_CPU_COUNT" -e GEM_HOME="$GEM_HOME" -e HOME=srv -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -w /srv -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)" -v "$(pwd)":/srv --hostname="$DOCKER_HOSTNAME" "$DOCKER_TAGS" bundle exec jekyll build
+    docker run --rm -t -i --cpus="$DOCKER_RUN_CPU_COUNT" -e GEM_HOME="$GEM_HOME" -e HOME=srv -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -w /srv -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)" -v "$(pwd)":/srv --hostname="$DOCKER_HOSTNAME" "$DOCKER_TAG" bundle exec jekyll build --verbose
 }
 
 # Build Docker iamge, rebuilding if necessary
-printf "Building Docker image %s" "$DOCKER_TAGS"
+printf "Building Docker image %s" "$DOCKER_IMAGE_NAME"
 _re_build_docker_image
 
 # Locally install gems
 docker_local_gem_install
 
-# # Build Jekyll site
+# # # Build Jekyll site
 docker_build_site
 
-# exit 0
+exit 0
