abc1dfda87cea4883889561e8fcf71bf2a3c35f5
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-08-06 17:09:05 +0100

    Home.js updated

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -15,6 +15,9 @@ $(document).ready(function () {
         loop: true,
         margin: 10,
         responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
         responsive: {
             0: {
                 items: 1,
@@ -25,7 +28,7 @@ $(document).ready(function () {
                 nav: false
             },
             1000: {
-                items: 3,
+                items: 4,
                 nav: true,
                 loop: false
             }
