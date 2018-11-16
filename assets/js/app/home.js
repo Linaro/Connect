16ceb9317e7e6fdc5f9d993bad17fef55121ef2f
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-11-16 15:05:07 +0000

    Added JS for lazyloading the promo video

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -31,4 +31,37 @@ $(document).ready(function () {
             }
         }
     });
+
+    if($("#video-holder").length > 0) {
+        // Get data-src attribute values - this is the youtube video link added from the page front matter in Jekyll
+        var url = $("#youtube-iframe").attr("data-src");
+        // Function to return the ID of a youtube video given the standard URL
+        if (url) {
+            function getId(url) {
+                var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+                var match = url.match(regExp);
+
+                if (match && match[2].length == 11) {
+                    return match[2];
+                } else {
+                    return 'error';
+                }
+            }
+            // Get the ID for the YouTube video.
+            var youtubeId = getId(url);
+            // Create the YouTube embed url
+            var embedUrl = "//www.youtube.com/embed/" + youtubeId;
+            // Set the src to the data-src
+            $("#youtube-iframe").attr("src", embedUrl);
+            // Set video Iframe to visible and remove the video-skeleton placeholder
+            // once the video has loaded with the s
+            $("#youtube-iframe").on("load", function () {
+                $("#video-embed").removeClass("hidden-iframe");
+                $(this).removeClass("hidden-iframe");
+                $("#video-skeleton").hide();
+                $(this).addClass("visible-iframe");
+                $("#video-embed").addClass("visible-iframe");
+            });
+        }
+    }
 });
