526b1bed990eacdd6fa43f68b3083bbfba57755a
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-12-12 16:06:43 +0000

    Updated Youtube video lazyloading js to remove iframe src when the Bootstrap modal closes

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -55,61 +55,32 @@ $(document).ready(function () {
         }
     });
 
-    // if($("#video-holder").length > 0) {
-    //     // Get data-src attribute values - this is the youtube video link added from the page front matter in Jekyll
-    //     var url = $("#youtube-iframe").attr("data-src");
-    //     // Function to return the ID of a youtube video given the standard URL
-    //     if (url) {
-    //         function getId(url) {
-    //             var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    //             var match = url.match(regExp);
-
-    //             if (match && match[2].length == 11) {
-    //                 return match[2];
-    //             } else {
-    //                 return 'error';
-    //             }
-    //         }
-    //         // Get the ID for the YouTube video.
-    //         var youtubeId = getId(url);
-    //         // Create the YouTube embed url
-    //         var embedUrl = "//www.youtube.com/embed/" + youtubeId;
-    //         // Set the src to the data-src
-    //         $("#youtube-iframe").attr("src", embedUrl);
-    //         // Set video Iframe to visible and remove the video-skeleton placeholder
-    //         // once the video has loaded with the s
-    //         $("#youtube-iframe").on("load", function () {
-    //             $("#video-embed").removeClass("hidden-iframe");
-    //             $(this).removeClass("hidden-iframe");
-    //             $("#video-skeleton").hide();
-    //             $(this).addClass("visible-iframe");
-    //             $("#video-embed").addClass("visible-iframe");
-    //         });
-    //     }
-    // }
-
-        var youtube = document.querySelectorAll( ".youtube" );
-        
-        for (var i = 0; i < youtube.length; i++) {
-            
-            var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
-
-            var image = new Image();
-                    image.src = source;
-                    image.addEventListener( "load", function() {
-                        youtube[ i ].appendChild( image );
-                    }( i ) );
-            
-                    youtube[i].addEventListener( "click", function() {
+    var youtube = document.querySelectorAll( ".youtube" );
     
-                        var iframe = document.createElement( "iframe" );
-    
-                                iframe.setAttribute( "frameborder", "0" );
-                                iframe.setAttribute( "allowfullscreen", "" );
-                                iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
-    
-                                this.innerHTML = "";
-                                this.appendChild( iframe );
-                    } );	
-        }
+    for (var i = 0; i < youtube.length; i++) {
+        var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+        var image = new Image();
+        image.src = source;
+        image.addEventListener( "load", function() {
+            youtube[ i ].appendChild( image );
+        }( i ) );
+
+        youtube[i].addEventListener( "click", function() {
+
+            var iframe = document.createElement( "iframe" );
+
+                    iframe.setAttribute( "frameborder", "0" );
+                    iframe.setAttribute( "allowfullscreen", "" );
+                    iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+                    $(".youtube img").hide();
+                    this.appendChild( iframe );
+        } );	
+    }
+
+        // Check for modal close event
+        $("#connectPromoVideo").on('hidden.bs.modal', function () {
+            $(".youtube iframe").attr('src', '');
+            $(".youtube img").show();
+        });
 });
