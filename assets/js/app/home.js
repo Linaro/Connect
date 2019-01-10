2768a3c0a71df0f793dfb3ab0fb83cc757e969b4
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-10 13:59:04 +0000

    Reverting JS back to original

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -55,31 +55,30 @@ $(document).ready(function () {
         }
     });
 
-    var youtube = document.querySelectorAll( ".youtube" );
-    
+    var youtube = document.querySelectorAll(".youtube");
+
     for (var i = 0; i < youtube.length; i++) {
-        var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+        var source = "https://img.youtube.com/vi/" + youtube[i].dataset.embed + "/sddefault.jpg";
         var image = new Image();
         image.src = source;
-        image.addEventListener( "load", function() {
-            youtube[ i ].appendChild( image );
-        }( i ) );
+        image.addEventListener("load", function () {
+            youtube[i].appendChild(image);
+        }(i));
 
-        youtube[i].addEventListener( "click", function() {
-            var iframe = document.createElement( "iframe" );
-            iframe.setAttribute( "frameborder", "0" );
-            iframe.setAttribute( "id", "promoVideo" );
-            iframe.setAttribute( "allowfullscreen", "" );
-            iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
-            this.appendChild( iframe );
+        youtube[i].addEventListener("click", function () {
+            var iframe = document.createElement("iframe");
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("id", "promoVideo");
+            iframe.setAttribute("allowfullscreen", "");
+            iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
+            this.appendChild(iframe);
             $(".youtube img").hide();
             $(".youtube .play-button").hide();
-        } );	
+        });
     }
 
     // Check for modal close event
     $(".closeVideo").on('click', function () {
-        e.stopPropagation();
         // Reload Iframe
         var iframe = document.getElementById("promoVideo");
         iframe.src = iframe.src;
@@ -87,17 +86,17 @@ $(document).ready(function () {
         console.log("Closed and paused.");
     });
 
-    $(window).click(function () {
-        // Reload Iframe
-        var iframe = document.getElementById("promoVideo");
-        iframe.src = iframe.src;
-        console.log($("#youtube-container").attr("data-embed"));
-        console.log("Closed and paused.");
-    });
+    // $(window).click(function () {
+    //     // Reload Iframe
+    //     var iframe = document.getElementById("promoVideo");
+    //     iframe.src = iframe.src;
+    //     console.log($("#youtube-container").attr("data-embed"));
+    //     console.log("Closed and paused.");
+    // });
 
-    $('.modal-dialog').click(function (event) {
-        event.stopPropagation();
-    });
+    // $('.modal-dialog').click(function (event) {
+    //     event.stopPropagation();
+    // });
 
 
 });
