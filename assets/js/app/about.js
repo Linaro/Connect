9a32c9b6ed62cd99e306b0c69c8183f2d6997e72
Kyle <kyle.kirkby@linaro.org>
2019-02-19 13:11:27 +0000

    Updated the BKK19 posts and added featured images - about page layout updated (#138)

--- assets/js/app/about.js
+++ assets/js/app/about.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:5,
+        margin:0,
+        dots: false,
         nav:false,
         lazyLoad: true,
         autoplay:true,
-        autoplayTimeout:5000,
+        autoplayTimeout:3000,
+        rewind: true,
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:1
+                items:3
             },
             1000:{
-                items:1
+                items:6
+            },
+            1200:{
+                items:8
             }
         }
     });
