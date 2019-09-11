6f42d9bca77cc2bfbcdfbfc4f456afd10d464927
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-09-11 13:40:56 +0100

    instantiated the owl carousel for testimonial's on the sponsor/ page

--- assets/js/app/sponsor.js
+++ assets/js/app/sponsor.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip({ container: 'body'})
     });
-    
+
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:5,
@@ -21,5 +21,29 @@ $(document).ready(function() {
                 items:1
             }
         }
+    });
+
+    var testimonialSlider = $("#testimonial_slider");
+
+    testimonialSlider.owlCarousel({
+      items: 4,
+      loop: false,
+      dots: false,
+      nav: true,
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 8000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 2
+        }
+      }
     });
 });
