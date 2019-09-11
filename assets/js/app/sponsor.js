ea64fd6af7b0b8fc6d3a70c3601d966c9fe51489
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-09-11 13:49:22 +0100

    Updated JS

--- assets/js/app/sponsor.js
+++ assets/js/app/sponsor.js
@@ -3,26 +3,6 @@ $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip({ container: 'body'})
     });
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:5,
-        nav:false,
-        lazyLoad: true,
-        autoplay:true,
-        autoplayTimeout:5000,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    });
-
     var testimonialSlider = $("#testimonial_slider");
 
     testimonialSlider.owlCarousel({
@@ -46,4 +26,5 @@ $(document).ready(function() {
         }
       }
     });
+
 });
