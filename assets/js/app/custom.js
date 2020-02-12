83ee213da835dd948408d870c5c1e2be55db26e0
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-12 09:52:58 +0000

    Updated the CoronaVirus warning notice to use the js-cookie lib

--- assets/js/app/custom.js
+++ assets/js/app/custom.js
@@ -1,28 +1,12 @@
 //= require vendor/js-cookie.min
 
 $(document).ready(function() {
-  function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1);
-      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-  }
-  var getCookieAccept;
-
-  getCookieAccept = getCookie("diseasesnotice");
-  if (getCookieAccept != "accept") {
+  var warning_cookie = Cookies.get("corona_virus_warning");
+  if (typeof warning_cookie === undefined) {
     $("#messageNotice").show();
   }
-
   $("#acceptNotice").click(function() {
     $("#messageNotice").hide();
-    var expire = new Date();
-    //Setting cookie expiry after 6 months
-    expire = new Date(expire.getTime() + 15552000000);
-    document.cookie = "diseasesnotice=accept; expires=" + expire;
+    Cookies.set("corona_virus_warning", "accepted");
   });
 });
