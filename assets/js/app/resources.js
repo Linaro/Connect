01db1c4c658129a0ee0f1647dad701b73bde2141
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-06-01 15:30:16 +0100

    Adding load more resources functionality. Resources are limited to 12 items until the end user clicks the 'Load More' button

--- assets/js/app/resources.js
+++ assets/js/app/resources.js
@@ -1,7 +1,10 @@
 var resources_json = [];
+var items_to_display = [];
 var current_event = "all";
 var current_type = "all";
 var current_track = "all";
+var current_number_of_items = 12;
+const items_step = 12;
 // Get Data for a given url
 function getData(ajaxurl, callbackFunction) {
   return $.ajax({
@@ -24,12 +27,35 @@ function delay(callback, ms) {
     }, ms || 0);
   };
 }
+// Load more items when the load_more button is clicked.
+function load_more_items() {
+  // Get items step
+  if (items_to_display.length > current_number_of_items + items_step) {
+    var last_item_index = current_number_of_items - 1 + items_step;
+  } else {
+    var last_item_index = items_to_display.length;
+  }
+  var items_to_show = items_to_display.slice(
+    current_number_of_items - 1,
+    last_item_index
+  );
+  // Loop over results items_to_show display
+  for (var i = 0; i < items_to_show.length; i++) {
+    var identifier = items_to_show[i]["identifier"];
+    // Set display:block style for all results
+    $('*[data-identifier="' + identifier + '"]').css("display", "block");
+  }
+}
+
 // Update the cols listed on the page using the resource page path
 // identifiers
 function update_cols() {
+  $("#load_more").show();
+  // reset the items to show variable.
+  current_number_of_items = 12;
   // Hide all cols and display those in the resources json
   $("[data-identifier]").css("display", "none");
-  var items_to_display = resources_json;
+  items_to_display = resources_json;
   // Fix for issue regarding filter function returning items that are not nested in the
   // fuse.js item object.
   items_to_display.filter((item, index) => {
@@ -59,8 +85,16 @@ function update_cols() {
   console.log("Search results that will display: block:", items_to_display);
   // Check that we have results first
   if (items_to_display.length > 0) {
+    // Check if items_to_display is greater than the initial items num.
+    // If less current_number_of_items var then hide the load more button
+    if (items_to_display.length > current_number_of_items) {
+      var loop_limit = current_number_of_items;
+    } else {
+      $("#load_more").hide();
+      var loop_limit = items_to_display.length;
+    }
     // Loop over results to display
-    for (var i = 0; i < items_to_display.length; i++) {
+    for (var i = 0; i < loop_limit.length; i++) {
       var identifier = items_to_display[i]["identifier"];
       // Set display:block style for all results
       $('*[data-identifier="' + identifier + '"]').css("display", "block");
@@ -80,6 +114,11 @@ $(document).ready(function () {
       keys: ["tracks", "event_id", "title", "summary", "speakers.name"],
       threshold: 0.3,
       findAllMatches: true,
+    });
+    // Handle load more button click
+    $("#load_more").click(function (e) {
+      e.preventDefault();
+      load_more_items();
     });
     // Handle search input keyup
     $("#searchQuery").keyup(
